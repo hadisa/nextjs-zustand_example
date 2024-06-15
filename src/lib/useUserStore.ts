47f2ac9b4165f802b96cc701f89d5efcd5db4468
Hadisa <afg.norozi@gmail.com>
2024-06-15 11:20:13 +0430

    typescript

--- src/lib/useUserStore.ts
+++ src/lib/useUserStore.ts
@@ -1,8 +1,19 @@
+import { User } from "@/type/type";
 import create from "zustand";
 
-const useUserStore = create((set) => ({
+type UserStore = {
+  users: User[];
+  addUser: (user: User) => void;
+  removeUser: (identityId: string) => void;
+  updateUser: (updatedUser: User) => void;
+};
+
+const useUserStore = create<UserStore>((set) => ({
   users: [],
-  addUser: (user) => set((state) => ({ users: [...state.users, user] })),
+  addUser: (user) =>
+    set((state) => ({
+      users: [...state.users, user],
+    })),
   removeUser: (identityId) =>
     set((state) => ({
       users: state.users.filter((user) => user.identityId !== identityId),
